f19f60af694f050d15201a6468a0a0d0ed3c90b0
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-05 08:39:14 +0000

    I beheld the wretch -- the miserable monster whom I had created
    git-svn-id: https://plugins.svn.wordpress.org/frankencookie/tags/1.0.0@692095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ frankencookie.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * frankencookie.php
+ *
+ * Copyright (c) 2013 "kento" Karim Rahimpur www.itthinx.com
+ *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This header and all notices must be kept intact.
+ *
+ * @author Karim Rahimpur
+ * @package frankencookie
+ * @since frankencookie 1.0.0
+ *
+ * Plugin Name: FrankenCookie
+ * Plugin URI: http://www.itthinx.com/plugins/frankencookie
+ * Description: FrankenCookie provides a widget that offers visitors an explanation about cookies being placed on their computer.
+ * Version: 1.0.0
+ * Author: itthinx
+ * Author URI: http://www.itthinx.com
+ * Donate-Link: http://www.itthinx.com
+ * License: GPLv3
+ */
+define( 'FCOOK_CORE_VERSION',  '1.0.0' );
+define( 'FCOOK_FILE',          __FILE__ );
+define( 'FCOOK_DIR',           WP_PLUGIN_DIR . '/frankencookie' );
+define( 'FCOOK_CORE_URL',      WP_PLUGIN_URL . '/frankencookie' );
+define( 'FCOOK_PLUGIN_DOMAIN', 'frankencookie' );
+
+/**
+ * FrankenCookie plugin main class - fires up things if we must.
+ */
+class FrankenCookie {
+
+	/**
+	 * Load the widget stuff.
+	 */
+	public static function init() {
+		require_once( FCOOK_DIR . '/class-frankencookie-widget.php' );
+	}
+}
+
+FrankenCookie::init();
