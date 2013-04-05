0ea95b5eb8f05edafb32a2df09257189448086b8
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-05 08:39:14 +0000

    I beheld the wretch -- the miserable monster whom I had created
    git-svn-id: https://plugins.svn.wordpress.org/frankencookie/trunk@692095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-frankencookie-widget.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ * class-frankencookie-widget.php
+ *
+ * Copyright (c) 2013 "kento" Karim Rahimpur www.itthinx.com
+ *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This header and all notices must be kept intact.
+ *
+ * @author Karim Rahimpur
+ * @package frankencookie
+ * @since frankencookie 1.0.0
+ */
+
+/**
+ * FrankenCookie Widget implementation.
+ */
+class FrankenCookie_Widget extends WP_Widget {
+
+	const TEN_YEARS = 315360000;
+
+	/**
+	 * Hooks on widgets_init to add our widget.
+	 */
+	public static function init() {
+		add_action( 'init', array( __CLASS__, 'wp_init' ) );
+		add_action( 'widgets_init', array( __CLASS__, 'widgets_init' ) );
+	}
+
+	/**
+	 * Sets the cookie when the message should be hidden and
+	 * redirects to clean up the URL. 
+	 */
+	public static function wp_init() {
+		if ( isset( $_GET['_wpnonce'] ) ) {
+			if ( wp_verify_nonce( $_GET['_wpnonce'], 'frankencookie' ) ) {
+				setcookie( 'frankencookie', 1, time() + self::TEN_YEARS );
+				$current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+				$redirect_url = remove_query_arg( '_wpnonce', remove_query_arg( 'frankencookie', $current_url ) );
+				wp_redirect( $redirect_url );
+				exit;
+			}
+		}
+	}
+
+	/**
+	 * Registers the widget.
+	 */
+	public static function widgets_init() {
+		register_widget( __CLASS__ );
+	}
+
+	private $default_title = '';
+	private $default_text = '';
+	private $default_hide = '';
+
+	/**
+	 * Widget constructor.
+	 */
+	public function __construct() {
+		$this->default_text = __( 'We use cookies to optimize your experience on our site and assume you\'re OK with that if you stay.', FCOOK_PLUGIN_DOMAIN );
+		$this->default_hide = __( 'OK, hide this message.', FCOOK_PLUGIN_DOMAIN );
+
+		$widget_options = array(
+			'classname' => 'frankencookie',
+			'description' => __( 'Text or HTML', FCOOK_PLUGIN_DOMAIN )
+		);
+
+		parent::__construct(
+			'frankencookie',
+			__( 'FrankenCookie', FCOOK_PLUGIN_DOMAIN ),
+			$widget_options
+		);
+	}
+
+	/**
+	 * Widget content renderer.
+	 * @see WP_Widget::widget()
+	 */
+	public function widget( $args, $instance ) {
+
+		$show = !isset( $_COOKIE['frankencookie'] ) ? 'true' : 'false';
+
+		extract( $args );
+		$title = apply_filters(
+			'frankencookie_widget_title',
+			empty( $instance['title'] ) ? $this->default_title : $instance['title'],
+			$instance,
+			$this->id_base
+		);
+		$text = apply_filters(
+			'frankencookie_widget_text',
+			empty( $instance['text'] ) ? $this->default_text : $instance['text'],
+			$instance
+		);
+		$hide = apply_filters(
+			'frankencookie_widget_hide',
+			empty( $instance['hide'] ) ? $this->default_hide : $instance['hide'],
+			$instance
+		);
+
+		echo $before_widget;
+		// Using Javascript to hide the message to avoid caching issues:
+		// With caching, the widget would be rendered for a visitor and after
+		// the visitor clicks the 'hide' link, the message would still appear.
+
+		if ( !empty( $title ) ) {
+			echo $before_title . $title . $after_title;
+		}
+		echo '<div class="message">';
+		echo $text;
+		echo '</div>';
+		echo '<div class="hide">';
+		$current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		echo sprintf(
+			__( '<a href="%s">%s</a>', FCOOK_PLUGIN_DOMAIN ),
+			wp_nonce_url( add_query_arg( 'frankencookie', 'set', remove_query_arg( 'frankencookie', $current_url ) ), 'frankencookie' ),
+			$hide
+		);
+		echo '<noscript>';
+		echo __( 'You have disabled Javascript, to hide this notice, Javascript must be enabled.', FCOOK_PLUGIN_DOMAIN );
+		echo '</noscript>';
+		echo '</div>';
+
+		if ( isset( $widget_id ) ) {
+
+			echo '<script type="text/javascript">';
+			echo 'if (document.cookie.indexOf("frankencookie") >= 0 ) {';
+			// We must look for elements by class (*) :
+			echo 'var elements = document.getElementsByTagName("*");';
+			echo 'for (i in elements) {';
+			echo 'if (typeof elements[i].className !== "undefined") {';
+			echo 'if (elements[i].className.indexOf("frankencookie") >= 0) {';
+			echo 'elements[i].style.display = "none";';
+			echo '}';
+			echo '}';
+			echo '}';
+			echo '}';
+			echo '</script>';
+
+			// (*) We can't use the ID because if the widget changes and we have caching in place, the wrong element
+			// would be searched for and the message would not be hidden:
+			//echo sprintf( 'var frankenCookieWidget = document.getElementById("%s");', $widget_id );
+			//echo 'if ( typeof frankenCookieWidget !== "undefined" ) {';
+			//echo 'frankenCookieWidget.style.display = "none";';
+			//echo '}';
+		}
+		echo $after_widget;
+	}
+
+	/**
+	 * Updates widget settings.
+	 * @see WP_Widget::update()
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		if ( current_user_can( 'unfiltered_html' ) ) {
+			$instance['text'] =  $new_instance['text'];
+		} else {
+			$instance['text'] = stripslashes( wp_filter_post_kses( addslashes( $new_instance['text'] ) ) );
+		}
+		$instance['hide'] = strip_tags( $new_instance['hide'] );
+		return $instance;
+	}
+
+	/**
+	 * Widget settings renderer.
+	 * @see WP_Widget::form()
+	 */
+	public function form( $instance ) {
+
+		$instance = wp_parse_args(
+			(array) $instance,
+			array(
+				'title' => $this->default_title,
+				'text'  => $this->default_text,
+				'hide'  => $this->default_hide
+			)
+		);
+
+		$title = strip_tags( $instance['title'] );
+		$text  = esc_textarea( $instance['text'] );
+		$hide  = strip_tags( $instance['hide'] );
+
+		echo '<p>';
+		echo '<label>';
+		echo __( 'Title', FCOOK_PLUGIN_DOMAIN );
+		echo sprintf(
+			'<input class="widefat" id="%s" name="%s" type="text" value="%s" />',
+			$this->get_field_id( 'title' ),
+			$this->get_field_name( 'title' ),
+			esc_attr( $title )
+		);
+		echo '</label>';
+		echo '<br/>';
+		echo '<span class="description">';
+		echo __( 'The widget\'s title.', FCOOK_PLUGIN_DOMAIN );
+		echo '</span>';
+		echo '</p>';
+
+		echo '<p>';
+		echo '<label>';
+		echo __( 'Message about cookies', FCOOK_PLUGIN_DOMAIN );
+		echo sprintf(
+			'<textarea class="widefat" rows="16" cols="20" id="%s" name="%s">%s</textarea>',
+			$this->get_field_id( 'text' ),
+			$this->get_field_name( 'text' ), $text
+		);
+		echo '</label>';
+		echo '<br/>';
+		echo '<span class="description">';
+		echo __( 'The text that explains that cookies are placed on the visitor\'s computer.', FCOOK_PLUGIN_DOMAIN );
+		echo '</span>';
+		echo '</p>';
+
+		echo '<p>';
+		echo '<label>';
+		echo __( 'Text to hide the message', FCOOK_PLUGIN_DOMAIN );
+		echo sprintf(
+				'<input class="widefat" id="%s" name="%s" type="text" value="%s" />',
+				$this->get_field_id( 'hide' ),
+				$this->get_field_name( 'hide' ),
+				esc_attr( $hide )
+		);
+		echo '</label>';
+		echo '<br/>';
+		echo '<span class="description">';
+		echo __( 'The text for the link that the visitor clicks to hide the widget.', FCOOK_PLUGIN_DOMAIN );
+		echo '</span>';
+		echo '</p>';
+	}
+}
+FrankenCookie_Widget::init();
---
+++ readme.txt
@@ -0,0 +1,109 @@
+=== FrankenCookie ===
+Contributors: itthinx
+Donate link: http://www.itthinx.com/plugins/frankencookie
+Tags: bureaucracy, compliance, cookie, cookie law, cookies, eu, eu cookie directive, eu cookie law, nonsense, law, privacy, regulation, stupid, useless 
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 1.0.0
+License: GPLv3
+
+FrankenCookie provides a widget that offers visitors an explanation about cookies being placed on their computer.
+
+== Description ==
+
+FrankenCookie provides a widget that offers visitors an explanation about cookies being placed on their computer.
+The widget provides a default text which can be customized. It also provides a button that visitors can click so that the widget does not appear again as long as the FrankenCookie is found when the visitor browses the site.
+
+_"Beware, for I am fearless and therefore powerful."_ - the monster
+
+### Feedback ###
+
+Feedback is welcome!
+
+If you need help, have problems, want to leave feedback or want to provide constructive criticism, please do so here at the [FrankenCookie plugin page](http://www.itthinx.com/plugins/frankencookie/).
+
+Please try to solve problems there before you rate this plugin or say it doesn't work. There goes a _lot_ of work into providing you with free quality plugins! Please appreciate that and help with your feedback. Thanks!
+
+#### Twitter ####
+
+[Follow @itthinx on Twitter](http://twitter.com/itthinx) for updates on this and other plugins.
+
+### Translations ###
+
+* If you would like to help translate the plugin, please get in touch at the [FrankenCookie plugin page](http://www.itthinx.com/plugins/frankencookie/).
+
+== Installation ==
+
+1. Upload or extract the `frankencookie` folder to your site's `/wp-content/plugins/` directory. You can also use the *Add new* option found in the *Plugins* menu in WordPress.  
+2. Enable the plugin from the *Plugins* menu in WordPress.
+3. Drag the FrankenCookie widget under *Appearance > Widgets* to a sidebar.
+4. Customize the widget's text if you want to.
+
+== Frequently Asked Questions ==
+
+= I have a question, where do I ask? =
+
+You can leave a comment at the [FrankenCookie plugin page](http://www.itthinx.com/plugins/frankencookie/).
+
+= Does it work with caching plugins? =
+
+Yes. FrankenCookie renders the content of the widget and hides it with Javascript that checks if the `frankencookie` cookie (*yummy*) is present.
+If it is found, it hides the widget's content.
+As what is rendered does not change, it doesn't matter whether a caching mechanism is used or not.
+What changes is the behaviour based on the cookie. Of course this will only work if the visitor has Javascript enabled.
+Those that don't will always see the message.
+
+= How does this impact my site's performance? =
+
+Tests with [P3](http://wordpress.org/extend/plugins/p3-profiler/) show that the plugin's execution time is about 1-2% of WordPress' core.
+Well, still a small price to pay compared to the abysmal nonsense of *some* regulations.
+
+= How can I style the widget? =
+
+The widget can be styled quite easily using CSS rules.
+
+- the widget's CSS class is `frankencookie` 
+- the message is wrapped in a `div` with class `message`
+- the link to hide the message is also in a div with class `hide`
+
+Example - show the message at a fixed position at the bottom of the page:
+
+`.frankencookie {
+    font-size: 11px;
+    margin-top: 2px;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    color: #f0f0f0;
+    background-color: #000;
+    z-index: 10000;
+}
+.frankencookie .message,
+.frankencookie .hide {
+    display: inline;
+    margin: 2px;
+}
+.frankencookie .hide a {
+    color: #fff;
+    padding: 2px;
+    font-weight: bold;
+}
+.frankencookie .hide a:hover {
+    background-color: #999;
+    color: #111;
+}`
+
+== Screenshots ==
+
+1. FrankenCookie Widget Settings
+2. Example FrankenCookie Widget Appearance
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Initial release.
